8491ea5714e0a7ff8cf55dec09c03aa9b65ec071
Shrawan13-glitch <Shrawan13-glitch@users.noreply.github.com>
2026-05-16 12:27:57 +0530

    Fix missing import for Box in SearchBar.kt

--- app/src/main/java/com.shrawan.mediyo/music/ui/component/SearchBar.kt
+++ app/src/main/java/com.shrawan.mediyo/music/ui/component/SearchBar.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
