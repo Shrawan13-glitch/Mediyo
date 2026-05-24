be77f943e23d41097b8bb3751bf4c79760997c5d
Shrawan13-glitch <Shrawan13-glitch@users.noreply.github.com>
2026-05-24 12:02:26 +0530

    Bump version to v1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.shrawan.mediyo"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
